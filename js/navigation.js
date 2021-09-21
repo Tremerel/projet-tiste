7a44e4be176fc9e02b51d994f157380e258ca824
Tremerel <84023901+Tremerel@users.noreply.github.com>
2021-09-21 16:49:24 +0200

    Add files via upload

--- js/navigation.js
+++ js/navigation.js
@@ -5,6 +5,8 @@ function changeImage(anything){
 function toggleMenu(){
     const menuToggle = document.querySelector('.menuToggle');
     const navigation = document.querySelector('.navigation');
+    const navContainer = document.querySelector('.navContainer');
     menuToggle.classList.toggle('active');
     navigation.classList.toggle('active');
+    navContainer.classList.toggle('active');
 }
